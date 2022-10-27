b3d98fbbf31d27788502598e2a0e8000e49c1a6c
Mr GedJack <arsenpoter7777777@gmail.com>
2022-10-27 13:38:27 +0300

    camera setting

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
@@ -7,15 +8,47 @@ public class CameraMove : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private float smoothSpeed;
     new private Transform camera;
+    private Camera cam;
+    private bool isLocked = false;
 
     private void Start()
     {
         camera = GetComponent<Transform>();
+        cam = GetComponent<Camera>();
     }
 
     Vector3 velocity = Vector3.zero;
     private void LateUpdate()
     {
+        if (isLocked) return;
+
         camera.position = new Vector3(Vector3.SmoothDamp(camera.transform.position, target.transform.position, ref velocity, smoothSpeed).x, 0, camera.position.z);
+    }
+
+    /// <summary> Change setting for camera. </summary>
+    public void SetCameraSettings(float fov = -1, bool lockMode = false, float posX = 0, float posY = 0)
+    {
+        if (fov != -1) cam.fieldOfView = fov;
+        if (posX != 0) camera.position = new Vector2(posX, camera.position.y);
+        if (posY != 0) camera.position = new Vector2(camera.position.x, posY);
+
+        isLocked = lockMode;
+    }
+
+    /// <summary> You can get info about cam </summary> <returns> CameraSettings object</returns>
+    public CameraSettings GetSettings()
+    {
+        CameraSettings set = new();
+        set.fov = cam.fieldOfView;
+        set.lockMode = isLocked;
+        set.position = camera.position;
+        return set;
+    }
+
+    public struct CameraSettings
+    {
+        public float fov;
+        public bool lockMode;
+        public Vector2 position;
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed; //speed applied to move formula
-    [SerializeField] private GameObject player; // gameObject to reach player`s transform
-    private Rigidbody2D playerBody; //assigned rigidbody
-    private BoxCollider2D boxCollider; //assigned boxCollider
     [SerializeField] private LayerMask layerMask; //layer mask to check what raycast collided with
+    [SerializeField] private BoxCollider2D GroundedBoxCollider;
+    private Rigidbody2D playerBody; //assigned rigidbody
     private Controls input; //assigned keymaps of inputSystem
     private int jumpValue; //isJumped
+
     private void Start()
     {
-        playerBody = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();    
+        playerBody = GetComponent<Rigidbody2D>();  
         input = new();
         input.Enable();
     }
     private void Update()
     {
-        jumpValue = (int)input.PC.Jump.ReadValue<float>(); //gets the jumpButton keymap value
+        jumpValue = (int)input.PC.Jump.ReadValue<float>();
     }
 
     private void FixedUpdate()
@@ -27,27 +26,15 @@ public class PlayerController : MonoBehaviour
         short x = (short)input.PC.MoveX.ReadValue<float>(); //reads the MoveX axis values to implement them in move formula 
         short moveXform = (short)(x * speed * Time.fixedDeltaTime);
 
-        if (isGrounded()) { playerBody.velocity = transform.up * jumpValue * 5f; } // regular jump
+        if (IsGrounded()) { playerBody.velocity = transform.up * jumpValue * 5f; } // regular jump
 
         playerBody.velocity = new Vector2(moveXform, playerBody.velocity.y); //move formula
 
-        if (moveXform < 0) { player.transform.localScale = new Vector2(-1, 1); } //changes player direction
-        else { player.transform.localScale = new Vector2(1, 1); }
+        if (moveXform < 0) { playerBody.transform.localScale = new Vector2(-1, 1); } //changes player direction
+        else { playerBody.transform.localScale = new Vector2(1, 1); }
     }
-    private bool isGrounded() 
+    private bool IsGrounded() 
     {
-        float extraHeight = .02f; //raycast height except radius of boxCollider
-        RaycastHit2D rcHit = Physics2D.Raycast(boxCollider.bounds.center, Vector2.down, boxCollider.bounds.extents.y + extraHeight, layerMask); //raycast
-        Color rayColor; //raycast color
-        if (rcHit.collider != null) //raycast debug
-        {
-            rayColor = Color.red;
-        }
-        else 
-        {
-            rayColor = Color.blue;
-        }
-        Debug.DrawRay(boxCollider.bounds.center, Vector2.down * (boxCollider.bounds.extents.y + extraHeight), rayColor);
-        return rcHit.collider != null; //returns t or f based on if grounded
+        return Physics2D.IsTouchingLayers(GroundedBoxCollider, layerMask.value);
     }
 }
