385e29764c57adee3b739eb80d5583f60c830a07
WildFore <thesanikplay@gmail.com>
2022-11-06 11:42:01 +0200

    Animation update

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,10 +6,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed; //speed applied to move formula
     [SerializeField] private LayerMask layerMask; //layer mask to check what raycast collided with
     [SerializeField] private Animator an; //plaer animator
-    [SerializeField] private BoxCollider2D GroundedBoxCollider;
     private Rigidbody2D playerBody; //assigned rigidbody
     private Controls input; //assigned keymaps of inputSystem
-    private int jumpValue; //isJumped
 
     private void Start()
     {
@@ -17,17 +15,12 @@ public class PlayerController : MonoBehaviour
         input = new();
         input.Enable();
     }
-    private void Update()
-    {
-        jumpValue = (int)input.PC.Jump.ReadValue<float>();
-    }
 
     private void FixedUpdate()
     {
         short x = (short)input.PC.MoveX.ReadValue<float>(); //reads the MoveX axis values to implement them in move formula 
         short moveXform = (short)(x * speed * Time.fixedDeltaTime);
 
-        if (IsGrounded()) { playerBody.velocity = transform.up * jumpValue * 5f; } // regular jump
         playerBody.velocity = new Vector2(moveXform, playerBody.velocity.y); //move formula
 
         if (moveXform > 0 || moveXform < 0) { an.SetBool("walk", true); }//animation trigger
@@ -35,9 +28,5 @@ public class PlayerController : MonoBehaviour
 
         if (moveXform < 0) { playerBody.transform.localScale = new Vector2(-1, 1); } //changes player direction
         else if(moveXform > 0) { playerBody.transform.localScale = new Vector2(1, 1); }
-    }
-    private bool IsGrounded() 
-    {
-        return Physics2D.IsTouchingLayers(GroundedBoxCollider, layerMask.value);
     }
 }
