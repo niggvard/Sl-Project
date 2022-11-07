85dc1f4a60301f52ccdb59218fe3ab2d35bcf06f
WildFore <thesanikplay@gmail.com>
2022-11-07 11:23:59 +0200

    Added camera smooth transiting in scenes

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float smoothSpeed;
+    [SerializeField] private AnimationCurve smooth; // animation curve lerp factor
     new private Transform camera;
     private Camera cam;
     private bool isLocked = false;
@@ -21,16 +19,17 @@ public class CameraMove : MonoBehaviour
     private void LateUpdate()
     {
         if (isLocked) return;
-
+        SetCameraSettings(camAngle : Mathf.Lerp(GetSettings().camAngle, 14, smooth.Evaluate(0.05f)), fov : Mathf.Lerp(GetSettings().fov, 25, smooth.Evaluate(0.05f))); 
         camera.position = new Vector3(Vector3.SmoothDamp(camera.transform.position, target.transform.position, ref velocity, smoothSpeed).x, camera.position.y, camera.position.z);
     }
 
     /// <summary> Change setting for camera. </summary>
-    public void SetCameraSettings(float fov = -1, bool lockMode = false, float posX = 0, float posY = 0)
+    public void SetCameraSettings(float fov = -1, bool lockMode = false, float posX = 0, float posY = 0, float camAngle = 0)
     {
         if (fov != -1) cam.fieldOfView = fov;
         if (posX != 0) camera.position = new Vector2(posX, camera.position.y);
         if (posY != 0) camera.position = new Vector2(camera.position.x, posY);
+        if (camAngle != 0) camera.rotation = Quaternion.Euler(0, 0, camAngle);
 
         isLocked = lockMode;
     }
@@ -42,6 +41,7 @@ public class CameraMove : MonoBehaviour
         set.fov = cam.fieldOfView;
         set.lockMode = isLocked;
         set.position = camera.position;
+        set.camAngle = camera.rotation.eulerAngles.z;
         return set;
     }
 
@@ -50,5 +50,6 @@ public class CameraMove : MonoBehaviour
         public float fov;
         public bool lockMode;
         public Vector2 position;
+        public float camAngle;
     }
 }
